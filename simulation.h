1dec2239cd991fc5c7dec235d1ac19b05b56ed7e
Fofone9 <mr.paa2004@gmail.com>
2024-11-18 19:01:26 +0300

    Сделано

---
+++ simulation.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "data.h"
+
+void simulation();
+float getFPS();
+void keyboard();
+void movePlayer();
+void gameObjectsSimulation(float);
+void cameraSimulation(float);
+void moveEnemy(shared_ptr<GameObject>& entity);
+void destroyIfPossible(glm::ivec2 pos, glm::ivec2 displacement, bool Decrease);
+void bombSimulation(double currentTime);
